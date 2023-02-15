14ac51a9bb8e65015c3b789b6b2f990a2c1a1fae
Ruka Okuyama <rukaokuyama@gmail.com>
2023-02-15 15:43:57 +0900

    done with yelp review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,72 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[ show edit update destroy ]
+
+  # GET /reviews or /reviews.json
+  def index
+    @reviews = Review.all
+  end
+
+  # GET /reviews/1 or /reviews/1.json
+  def show
+  end
+
+  # GET /reviews/new
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  # GET /reviews/1/edit
+  def edit
+  end
+
+  # POST /reviews or /reviews.json
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /reviews/1 or /reviews/1.json
+  def update
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to review_url(@review), notice: "Review was successfully updated." }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /reviews/1 or /reviews/1.json
+  def destroy
+    @review.destroy
+
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: "Review was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+    # Only allow a list of trusted parameters through.
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
